0953ac8d92f343ac9365430f8d9e439c1ab7e3c6
Rdss-rgb <drendarjosh@gmail.com>
2025-05-14 15:03:04 +0800

    v

--- .svelte-kit/generated/server/internal.js
+++ .svelte-kit/generated/server/internal.js
@@ -21,7 +21,7 @@ export const options = {
 		app: ({ head, body, assets, nonce, env }) => "<!doctype html>\n<html lang=\"en\">\n\n<head>\n\t<meta charset=\"utf-8\" />\n\t<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n\t<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n\t<link\n\t\thref=\"https://fonts.googleapis.com/css2?family=Great+Vibes&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap\"\n\t\trel=\"stylesheet\">\n\t<link rel=\"icon\" href=\"" + assets + "/favicon.png\" />\n\t<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css\"\n\t\tintegrity=\"sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==\"\n\t\tcrossorigin=\"anonymous\" referrerpolicy=\"no-referrer\" />\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n\t" + head + "\n</head>\n\n<body data-sveltekit-preload-data=\"hover\"\n\tclass=\"relative bg-white text-slate-800 text-sm sm:text-base flex flex-col min-h-screen\">\n\t<div class=\"inset-0 opacity-5 absolute z-[-1] bgGrid\"></div>\n\t<div style=\"display: contents\">" + body + "</div>\n</body>\n\n</html>",
 		error: ({ status, message }) => ""
 	},
-	version_hash: "3d1dct"
+	version_hash: "16lqrf9"
 };
 
 export async function get_hooks() {
